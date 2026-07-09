d4132225bb1780ece12fd1795680591cefa45427
JU4NSCV <147772404+JU4NSCV@users.noreply.github.com>
2026-07-09 12:01:45 -0500

    Creacion de views y templates

--- taller/trabajo/taller01/views.py
+++ taller/trabajo/taller01/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.db.models import Sum
+from .models import Edificio, Departamento
 
-# Create your views here.
+def index(request):
+    total_edificios = Edificio.objects.count()
+    total_departamentos = Departamento.objects.count()
+    
+    context = {
+        'total_edificios': total_edificios,
+        'total_departamentos': total_departamentos,
+    }
+    return render(request, 'index.html', context)
+
+def listar_edificios(request):
+    edificios = Edificio.objects.all()
+    context = {
+        'edificios': edificios
+    }
+    return render(request, 'listar_edificios.html', context)
+
+def listar_departamentos(request):
+    departamentos = Departamento.objects.all()
+    context = {
+        'departamentos': departamentos
+    }
+    return render(request, 'listar_departamentos.html', context)
